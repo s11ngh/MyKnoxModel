182837795da12ccc3e77a13e044e18a7e580f353
Ujjwal Singh <86053991+s11ngh@users.noreply.github.com>
2022-11-10 18:09:52 -0600

    added KnoxClass class

--- myknoxclass.py
+++ myknoxclass.py
@@ -1,4 +1,3 @@
-
 class Major:
     def __init__(self, major, requiredclasses):
         self.major = major
@@ -25,4 +24,36 @@ class Major:
         return (KnoxClass in self.reqiredclasses)
 
 
-a = Major("Computer Sci", {1})
+class KnoxClass:
+    def __init__(self, classname, majordept, period, faculty) -> None:
+        self.classname = classname
+        self.majordept = majordept
+        self.period = period
+        self.faculty = faculty
+
+        if type(classname) is not str:
+            raise TypeError("classname must be of type string")
+        if type(majordept) is not Major:
+            raise TypeError("majordept must be an instance of class Major")
+        if type(period) is int:
+            # i think knox has 8 periods (not sure)
+            if period > -1 or period < 9:
+                raise ValueError("Enter a period in range 0-8 (inclusive)")
+        else:
+            raise TypeError("period must be of type int")
+        if type(faculty) is not list:
+            raise TypeError("period must be of type list")
+
+    def periodchange(self, newperiod):
+        if type(newperiod) is int and newperiod < 9 and newperiod > -1:
+            self.period = newperiod
+        else:
+            raise ValueError("Enter a period within 0-8")
+
+    def add_faculty(self, facultyname):
+        if type(facultyname) is str:
+            self.faculty.append(facultyname)
+
+    def remove_faculty(self, facultyname):
+        if type(facultyname) is str:
+            self.faculty.remove(facultyname)
