54ca2af7b3db90138adea716b1ae6af66eecbd41
Ujjwal Singh <86053991+s11ngh@users.noreply.github.com>
2022-11-12 19:17:44 -0600

    Implement instructions in comments @ KnoxDatabase

---
+++ KnoxDatabase.py
@@ -0,0 +1,12 @@
+import myknoxclass
+"""
+    1. Store list of majors available at Knox (alphabetical)
+    2. Store names of professors (alphabetical prefrable)
+    3. Store classes offered at Knox College + Link w/ majors they come under
+    4. Store student list in directory database
+    5. Move functions from myknoxclass to here
+"""
+
+
+class KnoxDatabase:
+    pass
