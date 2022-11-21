8372e1826c970343527b922e8c0724e756ef182b
Ujjwal Singh <86053991+s11ngh@users.noreply.github.com>
2022-11-21 02:38:46 -0600

    Added functionality for creating student report pdf

--- KnoxDatabase.py
+++ KnoxDatabase.py
@@ -8,7 +8,8 @@ import myknoxclass
 """
 
 
-majors = dict()  # <Name of Major(Str), MajorReqList[KnoxClass]>
+majors = dict()  # <Name of Major(Str), MajorReqList[KnoxClass]>]
+majors['Undecided': []]
 studentdir = dict()
 profdir = dict()
 class_list = []
---
+++ StudentReport.py
@@ -0,0 +1,20 @@
+from reportlab.pdfgen.canvas import Canvas
+from myknoxclass import Student
+import KnoxDatabase
+
+
+def studentreport(self, student):
+    if type(student) is not Student:
+        raise TypeError("Student must be an instance of class student")
+
+    newpdf = Canvas(student.name+"report.pdf")  # create pdf
+    newpdf.setFont('Courier', 24)  # heading font
+    newpdf.drawString(60, 730, 'Student Report: '+student.name)  # heading
+    newpdf.line(60, 725, 360, 725)
+    newpdf.drawString(90, 850, 'Knox ID Number: '+student.knoxID)
+    newpdf.drawString(90, 950, 'Major: '+str(student.majordept))
+    newpdf.drawString(90, 1050, 'Email: '+student.email)
+    newpdf.drawString(90, 1150, 'Academic Record: ' +
+                      str(student.academicrecord))
+    newpdf.drawString(90, 1250, 'Advisor: '+str(student.advisor))
+    newpdf.save()
